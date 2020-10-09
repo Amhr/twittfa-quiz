2734175838213b5d818c288f5489422e6d46946c
Mohammad Hosseini Rad <mohammad.hosseinirad@cafebazaar.ir>
2020-10-09 19:38:30 +0330

    update list cache config

--- config/app.go
+++ config/app.go
@@ -3,5 +3,5 @@ package config
 import "time"
 
 const APP_DEFAULT_DISPLAY_NAME = "ناشناس"
-const APP_LIST_CACHE_TIME = time.Duration(1) * time.Second
+const APP_LIST_CACHE_TIME = time.Duration(30) * time.Second
 const APP_LIST_PER_PAGE = 10
